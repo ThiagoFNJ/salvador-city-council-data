e4375f20096a4fd734f14b02543ad3751da02c0d
Thiago Nogueira <thiagonogueira.if@gmail.com>
2022-06-17 22:04:01 -0300

    Update necessary libraries

--- scripts/get_data_cms.py
+++ scripts/get_data_cms.py
@@ -1,5 +1,8 @@
+from bs4 import BeautifulSoup as soup
+import pandas as pd
 import requests
-from bs4 import BeautifulSoup
+import re
 
 cms_website_request = requests.get("https://www.cms.ba.gov.br/transparencia/recursos-humanos")
 cms_website_parser = BeautifulSoup(cms_website_request.text, "html.parser")
+
